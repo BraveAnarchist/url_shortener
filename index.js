093642cf792dc2ce0aa164dff6ad0ded6aa80b68
Elton <anonymousunknown9856@gmail.com>
2024-09-08 22:07:00 +0530

    done

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
+var cors = require('cors')
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use(cors())
 
 const urlDatabase = {};
 
